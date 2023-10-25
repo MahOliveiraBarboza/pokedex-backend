7483c0d4d62f7ad02a305f428980b86ddf996919
Marcela Barboza <marcela.barboza@telefonica.com>
2023-10-25 11:01:20 -0300

    [BSBD-3120][BSBD-3439] refactor: correction in gateway file

--- src/main/java/com/pokemon/marcela/infrastructure/gateways/PokemonGateway.java
+++ src/main/java/com/pokemon/marcela/infrastructure/gateways/PokemonGateway.java
@@ -1,30 +1,29 @@
 package com.pokemon.marcela.infrastructure.gateways;
 
-// import javax.enterprise.inject.Produces;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-// import javax.ws.rs.QueryParam;
-// import javax.ws.rs.core.MediaType;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.Produces;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import com.pokemon.marcela.infrastructure.domain.PokemonDetail;
-import com.pokemon.marcela.infrastructure.domain.PokemonResponse;
+import com.pokemon.marcela.infrastructure.domain.PokemonListResponse;
 
 
-
-@Path("/")
+@Path("/api/v2/pokemon")
 @RegisterRestClient(baseUri = "https://pokeapi.co")
 public interface PokemonGateway {
 
     @GET
-    @Path("/api/v2/pokemon?limit=10&offset=0")
-    // @Produces(MediaType.APPLICATION_JSON)
-    PokemonResponse getAllPokemons();
+    @Produces(MediaType.APPLICATION_JSON)
+    PokemonListResponse getAllPokemons(@QueryParam("limit=10") @DefaultValue("10") String limit, @QueryParam("offset") @DefaultValue("0") String offset);
 
     @GET
-    @Path("/api/v2/pokemon/{pokemonName}")
-    // @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{pokemonName}")
+    @Produces(MediaType.APPLICATION_JSON)
     PokemonDetail getDetailPokemon(@PathParam("pokemonName") String pokemonName);
 }
