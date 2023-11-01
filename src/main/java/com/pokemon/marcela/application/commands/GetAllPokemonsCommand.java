0aa34cb905a1bd373e296d526cc0ab5362f70813
Marcela Barboza <marcela.barboza@telefonica.com>
2023-11-01 11:32:11 -0300

    [BSBD-3120][BSBD-3439] feat: Command Detail and List created

---
+++ src/main/java/com/pokemon/marcela/application/commands/GetAllPokemonsCommand.java
@@ -0,0 +1,36 @@
+package com.pokemon.marcela.application.commands;
+
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import com.pokemon.marcela.application.mapper.ListPokemonMapper;
+import com.pokemon.marcela.infrastructure.adapters.exception.GetPokemonException;
+import com.pokemon.marcela.infrastructure.domain.PokemonListResponse;
+import com.pokemon.marcela.infrastructure.domain.entities.PokemonListEntity;
+import com.pokemon.marcela.infrastructure.gateways.InterfaceListPokemonGateway;
+
+@ApplicationScoped
+public class GetAllPokemonsCommand {
+    private final InterfaceListPokemonGateway listPokemonGateway;
+    private final ListPokemonMapper listPokemonMapper;
+    private static final String LIMIT = "10";
+    private static final String OFFSET = "0";
+
+    @Inject
+    public GetAllPokemonsCommand(@RestClient InterfaceListPokemonGateway listPokemonGateway, ListPokemonMapper listPokemonMapper) {
+        this.listPokemonGateway = listPokemonGateway;
+        this.listPokemonMapper = listPokemonMapper;
+    }
+
+    public List<PokemonListEntity> execute() {
+        try {
+            PokemonListResponse pokemonListResponse = listPokemonGateway.getAllPokemons(LIMIT, OFFSET);
+            return listPokemonMapper.mapPokemonListResponseToPokemonListEntity(pokemonListResponse);
+         } catch (Exception e) {
+            throw new GetPokemonException("Não foi possível obter a lista de pokémons: " + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/pokemon/marcela/application/commands/GetDetailPokemonCommand.java
@@ -0,0 +1,31 @@
+package com.pokemon.marcela.application.commands;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import com.pokemon.marcela.application.mapper.DetailPokemonMapper;
+import com.pokemon.marcela.infrastructure.adapters.exception.GetPokemonException;
+import com.pokemon.marcela.infrastructure.domain.PokemonDetail;
+import com.pokemon.marcela.infrastructure.domain.entities.PokemonDetailEntity;
+import com.pokemon.marcela.infrastructure.gateways.InterfaceDetailPokemonGateway;
+
+@ApplicationScoped
+public class GetDetailPokemonCommand {
+    private final InterfaceDetailPokemonGateway pokemonGateway;
+    private final DetailPokemonMapper pokemonMapper;
+
+    @Inject
+    public GetDetailPokemonCommand(InterfaceDetailPokemonGateway pokemonGateway, DetailPokemonMapper pokemonMapper) {
+        this.pokemonGateway = pokemonGateway;
+        this.pokemonMapper = pokemonMapper;
+    }
+
+    public PokemonDetailEntity execute(String pokemonName) {
+        try {
+            PokemonDetail pokemonDetail = pokemonGateway.getDetailPokemon(pokemonName);
+            return pokemonMapper.mapPokemonDetailToPokemonEntity(pokemonDetail);
+        } catch (Exception e) {
+            throw new GetPokemonException("Não foi possível obter o card de detalhes do pokémon: " +e.getMessage());
+        }
+    }
+}
